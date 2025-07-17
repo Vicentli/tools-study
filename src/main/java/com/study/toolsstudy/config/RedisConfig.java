f4dda03ec6d8f2c9027ad547bf0de73ffeea7b40
Vicentli <qq2268253110@163.com>
2025-07-17 18:32:13 +0800

    支持pgsql启动，使用mybatis替代hibernate

--- src/main/java/com/study/toolsstudy/config/RedisConfig.java
+++ src/main/java/com/study/toolsstudy/config/RedisConfig.java
@@ -30,11 +30,10 @@ public class RedisConfig {
         template.setConnectionFactory(connectionFactory);
 
         // 使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
-        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
         objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(objectMapper, Object.class);
 
         // 使用StringRedisSerializer来序列化和反序列化redis的key值
         StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
--- src/main/java/com/study/toolsstudy/config/SecurityConfig.java
+++ src/main/java/com/study/toolsstudy/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests((requests) -> requests
                 .anyRequest().permitAll()
             );
